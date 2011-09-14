6da802617707ebcca3361bb29d685d85572b963c
Gregory Koberger <gkoberger@gmail.com>
2011-09-14 10:23:10 -0700

    More stuff.. I'll try to be better about versioning later

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var etch = $('#etch canvas')[0],
     context = etch.getContext('2d'),
-    cur_x = 155.5,
-    cur_y = 75.5,
-    rot_x = rot_y = 0;
+    cur_x = 155.5, cur_y = 75.5, /* Center-ish */
+    rot_x = rot_y = 0,
+    path = [];
 
 context.strokeStyle = "rgba(90,90,90,0.9)";
 context.lineWidth = 0.5;
@@ -24,20 +24,22 @@ function drawLine(x, y) {
   context.stroke();
 }
 
-$(window).keypress(function(e) {
-  //right = 39, left = 37, up = 38, down = 40
-  if(e.keyCode == 39) {
-    drawLine(1, 0);
-  } else if(e.keyCode == 37) {
-    drawLine(-1, 0);
-  } else if(e.keyCode == 38) {
-    drawLine(0, -1);
-  } else if(e.keyCode == 40) {
-    drawLine(0, 1);
-  } else {
-    return;
-  }
-  e.preventDefault();
+$(function() {
+  $(document).keydown(function(e) {
+    if(e.keyCode == jQuery.ui.keyCode.RIGHT) {
+      drawLine(1, 0);
+    } else if(e.keyCode == jQuery.ui.keyCode.LEFT) {
+      drawLine(-1, 0);
+    } else if(e.keyCode == jQuery.ui.keyCode.UP) {
+      drawLine(0, -1);
+    } else if(e.keyCode == jQuery.ui.keyCode.DOWN) {
+      drawLine(0, 1);
+    } else {
+      // Not an arrow key; carry on!
+      return;
+    }
+    e.preventDefault();
+  });
 });
 
 /* Shake and bake */
@@ -47,22 +49,23 @@ var total_x = total_y = 0;
 var timer = false;
 
 $( "#etch" ).draggable({ revert: true, scroll:false, start: function(){
-  timer = setInterval(function(){ total_x--; total_y--;
-
-                      if(total_x < 0) total_x = 0;
-                      if(total_y < 0) total_y = 0;
+  timer = setInterval(function(){
+    total_x--;
+    total_y--;
+    if(total_x < 0) total_x = 0;
+    if(total_y < 0) total_y = 0;
   }, 300);
 },
 drag: function(e){
-  var new_direction_x = e.pageX - pos_x > 0 ? 1 : -1;
-  var new_direction_y = e.pageY - pos_y > 0 ? 1 : -1;
-  if(new_direction_x != direction_x) {
+  var new_direction_x = e.pageX - pos_x > 0 ? 1 : -1,
+      new_direction_y = e.pageY - pos_y > 0 ? 1 : -1;
+  if(Math.abs(e.pageX - pos_x) > 10 && new_direction_x != direction_x) {
     direction_x = new_direction_x;
     total_x++;
   }
-  if(new_direction_y != direction_y) {
-    total_y++;
+  if(Math.abs(e.pageY - pos_y) > 10 && new_direction_y != direction_y) {
     direction_y = new_direction_y;
+    total_y++;
   }
 
   pos_x = e.pageX;
